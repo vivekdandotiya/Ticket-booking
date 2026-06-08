d30743d61aaa3c4c8a43084ac81197c8659a8002
Developer <developer@cinepass.com>
2026-06-08 15:46:38 +0530

    fix(frontend): import MovieCard in Index.tsx

--- movie-booking-frontend/src/pages/Index.tsx
+++ movie-booking-frontend/src/pages/Index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import HeroCarousel from "../components/HeroCarousel";
+import MovieCard from "../components/MovieCard";
 import MovieSection from "../components/MovieSection";
 import { Movie } from "../types";
 import { api } from "../lib/api";
