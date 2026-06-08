c2cdfcde9a74f652b89f6fb6b354d207a5a2e58d
Developer <developer@cinepass.com>
2026-06-08 15:36:50 +0530

    fix(frontend): import Link from react-router-dom in Index.tsx

--- movie-booking-frontend/src/pages/Index.tsx
+++ movie-booking-frontend/src/pages/Index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import HeroCarousel from "../components/HeroCarousel";
 import MovieSection from "../components/MovieSection";
